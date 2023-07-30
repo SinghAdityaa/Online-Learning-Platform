39baec2e9dd8eaebef58b5d9d0a2ca2dcb6fe422
Prashant Dubey <dubeyprashant583@gmail.com>
2023-07-30 16:19:39 +0530

    handle form submission

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('user-registration-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  form.checkValidity()
+  form.reportValidity()
+
+
+  window.location = "/index.html" 
+  
+}
+)
