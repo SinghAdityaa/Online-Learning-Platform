9b70ec8c54c51c77a1d3cd09fc408e5d75bc1ecb
Aditya Singh <AdityaSingh2962@gmail.com>
2023-08-08 16:15:22 +0530

    add menu botton

--- main.js
+++ main.js
@@ -667,4 +667,15 @@ const courses = [
   
  
   renderCourses(courses);
+
+  // Toggle visibility of dropdown content
+function toggleDropdown() {
+  var dropdownContent = document.getElementById('dropdown-content');
+  if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';   
+  } else {
+      dropdownContent.style.display = 'block';
+  }
+}
+
   
