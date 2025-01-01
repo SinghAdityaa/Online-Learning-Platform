a2d25ec0adb3ecd7e7a6d86169537370651050af
Aditya Singh <singhaditya2962@gmail.com>
2025-01-01 19:34:13 +0530

    fix some errors

--- courses.js
+++ courses.js
@@ -112,7 +112,6 @@ const courses = [
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
